4f4758bcf996bb598da02a0d429bddf7cf5a5ef9
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-03-06 11:18:00 +0200

    Add files via upload

---
+++ ConsoleApp2/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+        private double z;
+        private double e;
+        private static int count;
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+        public double E
+        {
+            get { return e; }
+            set { e = value; }
+        }
+        public static int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public Point()
+        {
+            X = 1.0;
+            Y = 1.0;
+            Z = 1.0;
+            E = 1.0;
+
+            Count++;
+        }
+        static Point()
+        {
+            Count = 0;
+        }
+        public Point(double x, double y, double z, double e)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.E = e;
+            Count++;
+        }
+        public override string ToString()
+        {
+            return $"x= {x} y= {y} z={z} e={e}\n{count}";
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[4];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine("Введите координаты {0}-й точки", i + 1);
+                double x = Convert.ToDouble(Console.ReadLine());
+                double y = Convert.ToDouble(Console.ReadLine());
+                double z = Convert.ToDouble(Console.ReadLine());
+                double e = Convert.ToDouble(Console.ReadLine());
+                points[i] = new Point(x, y, z, e);
+                    Console.ReadKey();
+            }
+        }
+    }
+}
